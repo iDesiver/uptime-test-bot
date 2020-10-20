2d423b168cb3ef844de52fa25366a38514a0c111
Desiver <lucas.janczak2@gmail.com>
2020-10-20 19:02:44 -0300

    Init

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 const { Client } = require("discord.js")
 const client = new Client()
-require("dotenv")
+require("dotenv").config()
+const ascii = require("ascii-table")
+const AsciiTable = require("ascii-table/ascii-table")
 
-client.on("ready", () => console.log("Up!") )
+client.on("ready", () => console.log(`Up at ${client.user.tag} (${client.user.id})!`) )
 
 client.on("message", async message => {
-    const time = () => new Date() - client.readyAt
-    if(!message.content === "uptime") return
-    await message.channel.send(`I've been up for ${time/1000*60*60*24}days, ${time/1000*60*60}hours and ${time/1000*60}minutes`)
+    if(message.author.bot) return
+    if(message.content !== "uptime" || message.author.id !== "203838646426664960") return
+    
+    const uptime = new Date(new Date() - client.readyAt)
+    const table = new ascii()
+                        .setTitle("Uptime")
+                        .setTitleAlign(AsciiTable.CENTER)
+                        .setBorder('║', '═', '╬', '╬')
+                        .setHeading("Time", "Measure")
+                        .setAlign(0, AsciiTable.CENTER)
+
+    if(uptime.getUTCHours() > 0) table.addRow(uptime.getUTCHours(), `Hour${uptime.getUTCHours()>1?"s":""}`)
+    if(uptime.getUTCMinutes() > 0) table.addRow(uptime.getUTCMinutes(), `Minute${uptime.getUTCMinutes()>1?"s":""}`)
+    if(uptime.getUTCSeconds() > 0) table.addRow(uptime.getUTCSeconds(), `Second${uptime.getUTCSeconds()>1?"s":""}`)
+    
+       
+    await message.channel.send(table.toString(), {code: true})
 })
 
 
